4f71db9153102461e40c7f6bdbca22bbbbae36dc
benybodipo <benybodipo@gmail.com>
2023-05-31 11:04:48 +0200

    Connect to a differenct API endpoint

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const commentsRouter = require('./routes/comments');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT||5000;
+
+
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');  
@@ -11,3 +14,4 @@ app.use('/comments', commentsRouter);
 
 
 app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
--- routes/comments.js
+++ routes/comments.js
@@ -1,14 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+const fetch = require('node-fetch');
 
 
 const fetchComments = async (req, res, next) => {
     try {
-        const url = 'http://borolong.hyperphp.com/api/comments';
-        const response = await axios.get(url);
-        
-        return response.data;
+        const url = process.env.API_URL;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (typeof data === 'string') {
+            await fetchComments();
+        }
+        return data;
     } catch (error) {
         return error;
     }
@@ -17,7 +24,7 @@ const fetchComments = async (req, res, next) => {
 
 router.get('/', async (req, res) => {
     const comments = await fetchComments();
-
+    
     /**
      * Pagination
      */
@@ -26,14 +33,14 @@ router.get('/', async (req, res) => {
     const skip = (page - 1) * limit;
     const data = (comments.length) ? comments.slice(skip, skip + limit) : comments;
     const totalPages = (comments.length) ? Math.ceil(comments.length / limit) : 0;
-
+    
     res.render('index', {comments: data, page, totalPages, totalRecords: comments.length});
 });
 
 router.get('/:id', async (req, res) => {
     
     try {
-        const url = `http://borolong.hyperphp.com/api/comments/${req.params.id}`;
+        const url = `${process.env.API_UR}/${req.params.id}`;
         const response = await axios.get(url);
         
         const comment = response.data;
